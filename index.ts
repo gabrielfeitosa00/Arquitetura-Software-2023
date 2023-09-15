e2ab05c843bbc234342ac8f5813435e44fbe8888
Gabriel Perino Feitosa <50030104+gabrielfeitosa00@users.noreply.github.com>
2023-09-15 13:47:56 -0300

    refactor: less lines

--- index.ts
+++ index.ts
@@ -4,16 +4,18 @@ import Reporter from './src/Reporter';
 
 const [cmd, filename, format] = process.argv;
 console.log(format);
-
-if (format === 'html') {
-  let reporter = new Reporter(new ReporterHTML());
- 
-  let html = reporter.executeReporter('./data/cidades-2.json');
-  console.log(html);
-} else if (format === 'txt') {
-  let reporter = new Reporter(new ReporterTxt());
-  let txt = reporter.executeReporter('./data/cidades-2.json');
-  console.log(txt);
-} else {
+const formaterStrategies = {
+  'html': new ReporterHTML(),
+  'txt': new ReporterTxt()
+}
+try {
+  let reporter = new Reporter(
+    formaterStrategies[format]
+   ),
+   output = reporter.executeReporter(filename);
+   console.log(output);
+} catch (error) {
   console.log('Formato inválido!')
 }
+
+ 
