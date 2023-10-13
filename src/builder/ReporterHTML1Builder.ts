33fffa4cdbca5fbc0d2a65fad2131a5880eab2e9
Gabriel Perino Feitosa <50030104+gabrielfeitosa00@users.noreply.github.com>
2023-10-13 12:32:58 -0300

    feat: html builder

---
+++ src/builder/ReporterHTML1Builder.ts
@@ -0,0 +1,58 @@
+import FileReader from "../FilReader";
+import CSVParserDecorator from "../decorators/CSVParserDecorator";
+import IParser from "../decorators/IParser";
+import JSONParserDecorator from "../decorators/JSONParserDecorator";
+import IReporterBuilder from "./IReporterBuilder";
+import IStrategyParams from "./IStrategyParams";
+
+export default class ReporterHTML1Builder implements IReporterBuilder{
+    private fileReader:FileReader
+    private strategies: Array<IStrategyParams>
+    private parser: IParser
+constructor(fileReader: FileReader,parser: IParser,strategies: IStrategyParams[]){
+    this.fileReader = fileReader
+    this.parser =  parser
+    this.strategies = strategies
+}
+    dataOutput(data: string[]): void {
+        let result = `
+        <!DOCTYPE HTML>
+        <html>
+          <head>
+            <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+            <title>Relatório de Nomes de Cidades</title>
+          </head>
+          <body>
+            <h1>Relatório de Nomes de Cidades</h1>
+            <ul>
+        `;
+      
+          for (let i = 0; i < data.length; i++) {
+            result += '     <li>' + data[i]['Nome'] + '</li>\n';
+          }
+      
+          result += `
+            </ul>
+          </body>
+        </html>`;
+    
+        console.log(result)
+      
+    }
+    dataInput(): string[] {
+        for(const strategy of this.strategies){
+            const data = this.fileReader.readFile(strategy.path)
+            switch(strategy.fileType){
+                case  'JSON':
+                    this.parser = new JSONParserDecorator(this.parser,data)
+                    break;
+                case 'CSV':
+                    this.parser = new CSVParserDecorator(this.parser,data)
+                    break;
+                default:
+                    console.log(`Unsupported extension, the file at ${strategy.path} won't be read`)
+            }
+        }
+        return this.parser.parseData()
+    }
+}
